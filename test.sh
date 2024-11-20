45b81013ac6e093f0a387bd064c6f4f479b4e37b
JhDbacd <lorraime89@gmail.com>
2024-11-20 15:39:37 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-curl -L -o miui_update.zip "https://ultimateota.d.miui.com/OS1.0.15.0.UNQEUXM/miui_GOLDEEAGlobal_OS1.0.15.0.UNQEUXM_767a754131_14.0.zip?t=1732126804&s=d5432f295c023f7dfc0485223c772471"
+curl -L -o miui_update.zip "https://ultimateota.d.miui.com/OS1.0.15.0.UNQEUXM/miui_GOLDEEAGlobal_OS1.0.15.0.UNQEUXM_767a754131_14.0.zip?t=1732126804&s=d5432f295c023f7dfc0485223c772471" > /dev/null
 unzip -l miui_update.zip
